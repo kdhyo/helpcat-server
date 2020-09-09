90cb3ef31e2f7c6703e9334cfc6584167a4df75b
hyo <kdhyo98@gmail.com>
2020-09-09 23:12:36 +0900

    userall read

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,16 @@ function hello(_, { name }) {
   return `Hello ${name || "World"}`;
 }
 
+async function userAll(parent, args, context, info) {
+  try {
+    const users = await context.prisma.user.findMany({});
+    return users;
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
 module.exports = {
   hello,
+  userAll,
 };

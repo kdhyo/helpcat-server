6b69b569a780d4f90e6e5a87e1aad0c983d76cc6
hyo <kdhyo98@gmail.com>
2020-09-14 20:15:39 +0900

    utils ADD

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
-const APP_SECRET = "Help-catServer";
+import { APP_SECRET, getUserId } from "../utils";
 
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10);
@@ -22,9 +21,34 @@ async function signup(parent, args, context, info) {
   } catch (error) {
     return new Error(error);
   }
-  // const token = jwt.sign({ userId: user.userid }, APP_SECRET);
 }
+
+async function login(parent, args, context, info) {
+  const user = await context.prisma.user.findOne({
+    where: {
+      userid: args.userid,
+    },
+  });
+  if (!user) return new Error("아이디가 존재하지 않습니다.");
+
+  const pwdCheck = await bcrypt.compare(args.password, user.password);
+  if (!pwdCheck) return new Error("비밀번호가 올바르지 않습니다.");
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    APP_SECRET
+  );
+  return {
+    token,
+    user,
+  };
+}
+
+async function pwdChange(parent, args, context, info) {}
 
 module.exports = {
   signup,
+  login,
 };

ce1ab4702c9d86b82dc914b4c3d53a90dc933fdb
hyo <kdhyo98@gmail.com>
2020-09-14 20:44:59 +0900

    email인증 구현 중 part1

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  userMail: "kdhyo98@gmail.com", // User gmail
+  passMail: "!a759841265", // pass gmail
+};
---
+++ src/emailGenerator.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+import config from "./config/config";
+
+async function sendWelcomeEmail(user, context) {
+  const mailer = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: config.userMail,
+      pass: config.passMail,
+    },
+  });
+
+  const mailOptions = {
+    to: user.email,
+    from: config.userMail,
+    subject: "HELP CAT 이메일 인증",
+    html: `
+      <div>hello ${user.name}</div>
+      <div>Welcome in the HELP CAT.</div>
+        <div>Please find link to validate your email.
+           ${ctx.request.headers.origin}/validateEmail?validateEmailToken=${user.validateEmailToken}
+        </div>
+    `,
+  };
+  return mailer.sendMail(mailOptions);
+}

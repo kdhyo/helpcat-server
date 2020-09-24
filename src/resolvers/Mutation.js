dc00b7b512ffe3e41601d2d278b004cad902d1c9
dongHyo <kdhyo98@gmail.com>
2020-09-25 05:40:06 +0900

    서비스 수정기능 ADD

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -42,12 +42,47 @@ async function serviceDelete(parent, args, { request, prisma }, info) {
     });
     if (!service) return new Error("게시글이 존재하지 않습니다.");
 
-    // const service = await prisma.$exists.service({id, user})
-
     //게시글 아이디와 로그인된 아이디가 같은지 확인
     if (service.reqUserId === userId) {
       await prisma.service.delete({
         where: { id },
+      });
+      return true;
+    } else {
+      return new Error("게시글 작성자가 아닙니다.");
+    }
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
+// 서비스 글 수정
+async function editService(parent, args, { request, prisma }, info) {
+  try {
+    const user = isAuthenticated(request.res.req);
+    const userId = user.id;
+    const { id, title, contents, address, price, startAt, endAt } = args;
+
+    // 존재하는 게시글인지 확인.
+    const service = await prisma.service.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!service) return new Error("게시글이 존재하지 않습니다.");
+
+    //게시글 아이디와 로그인된 아이디가 같은지 확인
+    if (service.reqUserId === userId) {
+      await prisma.service.update({
+        where: { id },
+        data: {
+          title,
+          contents,
+          address,
+          price,
+          startAt,
+          endAt,
+        },
       });
       return true;
     } else {
@@ -66,4 +101,5 @@ module.exports = {
   validateEmail,
   serviceUproad,
   serviceDelete,
+  editService,
 };

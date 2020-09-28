19a31b6fb9704c807302718c2fdd2e06716eb836
hyo <kdhyo98@gmail.com>
2020-09-28 23:20:10 +0900

    이메일 인증 수정 진행중

---
+++ src/api/User/emailAuth/emailAuth.js
@@ -0,0 +1,37 @@
+export default {
+  Mutation: {
+    emailAuth: async (_, args, { prisma }) => {
+      const { validateEmailToken } = args;
+      try {
+        const userCheck = await prisma.User.findOne({
+          where: {
+            validateEmailToken,
+          },
+        });
+
+        if (!userCheck) {
+          return new Error(`존재하지 않는 유저 입니다.`);
+        } else {
+          if (userCheck.emailvalidated) {
+            return new Error(`올바르지 않은 접근입니다.`);
+          }
+        }
+      } catch (error) {
+        return ` try ${error}`;
+      }
+
+      try {
+        await prisma.User.update({
+          where: {
+            validateEmailToken: args.validateEmailToken,
+          },
+          data: { emailvalidated: true },
+        });
+
+        return true;
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};

1484484804a5241d0d6f175815c573bbb111ee23
hyo <kdhyo98@gmail.com>
2020-09-30 01:00:57 +0900

    이름 변경

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -1,8 +1,8 @@
 export default {
   User: {
-    reqServices: async ({ id }, __, { prisma }) =>
+    reqUser: async ({ id }, __, { prisma }) =>
       await prisma.service.findMany({ where: { reqUserId: id } }),
-    ansServices: async ({ id }, __, { prisma }) =>
+    ansUser: async ({ id }, __, { prisma }) =>
       await prisma.service.findMany({ where: { ansUserId: id } }),
   },
 };

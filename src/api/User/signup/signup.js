30bb6d2cb033814bffad18d4d18367d4f6508f81
hyo <kdhyo98@gmail.com>
2020-10-02 00:08:11 +0900

    이미 존재하는 유저 확인 로직 Delete

--- src/api/User/signup/signup.js
+++ src/api/User/signup/signup.js
@@ -5,15 +5,7 @@ export default {
     signup: async (_, args, { prisma }) => {
       // 패스워드 암호화 작업
       const password = await bcrypt.hash(args.password, 10);
-
       try {
-        const check = await prisma.user.count({
-          where: {
-            OR: [{ email: args.email }, { nickName: args.nickName }],
-          },
-        });
-        if (check) return new Error("이메일 혹은 닉네임이 이미 존재합니다.");
-
         const user = await prisma.user.create({
           data: {
             ...args,

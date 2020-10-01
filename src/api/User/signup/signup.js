774541829a77946910ae1247b928f67d46842878
hyo <kdhyo98@gmail.com>
2020-10-02 00:05:19 +0900

    회원가입 후 이메일인증 부분 삭제 및 회원가입 후 자동 로그인

--- src/api/User/signup/signup.js
+++ src/api/User/signup/signup.js
@@ -1,12 +1,11 @@
 import bcrypt from "bcryptjs";
-import { sendWelcomeEmail } from "../../../config/emailGenerator";
 
 export default {
   Mutation: {
     signup: async (_, args, { prisma }) => {
+      // 패스워드 암호화 작업
       const password = await bcrypt.hash(args.password, 10);
-      const validateEmailToken = Math.random().toString(36).substr(2, 8);
-      console.log(prisma);
+
       try {
         const check = await prisma.user.count({
           where: {
@@ -14,15 +13,15 @@ export default {
           },
         });
         if (check) return new Error("이메일 혹은 닉네임이 이미 존재합니다.");
+
         const user = await prisma.user.create({
           data: {
             ...args,
             password,
-            validateEmailToken,
           },
         });
-        // await sendWelcomeEmail(user);
-        return true;
+
+        return user;
       } catch (error) {
         return error;
       }

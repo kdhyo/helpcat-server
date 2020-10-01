07a9c41a9466693136844794ae791e29e39ed142
hyo <kdhyo98@gmail.com>
2020-10-01 16:58:58 +0900

    rooms 정보가 없을 시 예외처리

--- src/api/Room/seeRooms/seeRooms.js
+++ src/api/Room/seeRooms/seeRooms.js
@@ -8,6 +8,8 @@ export default {
           where: { userId: user.id },
         });
 
+        if (!rooms) return new Error("채팅방 정보가 존재하지 않습니다.");
+
         console.log(rooms);
         return rooms;
       } catch (error) {

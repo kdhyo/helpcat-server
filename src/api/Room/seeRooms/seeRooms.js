4298025ba34140fd9d884a7c28aa5d4f68fe901a
hyo <kdhyo98@gmail.com>
2020-10-01 16:57:57 +0900

    try catch 예외처리 적용

--- src/api/Room/seeRooms/seeRooms.js
+++ src/api/Room/seeRooms/seeRooms.js
@@ -3,12 +3,16 @@ export default {
     seeRooms: async (_, __, { prisma, isAuthenticated, request }) => {
       const user = isAuthenticated(request.res.req);
 
-      const rooms = await prisma.userOnRoom.findMany({
-        where: { userId: user.id },
-      });
+      try {
+        const rooms = await prisma.userOnRoom.findMany({
+          where: { userId: user.id },
+        });
 
-      console.log(rooms);
-      return rooms;
+        console.log(rooms);
+        return rooms;
+      } catch (error) {
+        return error;
+      }
     },
   },
 };

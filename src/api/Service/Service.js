26ae8ec96d52ac0074f0c87fc88bd650b3e91f89
hyo <kdhyo98@gmail.com>
2020-10-06 03:18:14 +0900

    service in serviceimgfiles view ADD

--- src/api/Service/Service.js
+++ src/api/Service/Service.js
@@ -4,5 +4,7 @@ export default {
       await prisma.service.findOne({ where: { id } }).reqUser(),
     ansUser: async ({ id }, __, { prisma }) =>
       await prisma.service.findOne({ where: { id } }).ansUser(),
+    serviceimgfiles: async ({ id }, __, { prisma }) =>
+      await prisma.serviceimgfiles.findMany({ where: { serviceId: id } }),
   },
 };

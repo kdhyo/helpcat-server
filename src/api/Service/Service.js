0c6a13aaec5b3e0b01f0afe8574457577d5d25ce
hyo <kdhyo98@gmail.com>
2020-10-06 20:44:27 +0900

    service in Rating 조회

--- src/api/Service/Service.js
+++ src/api/Service/Service.js
@@ -6,5 +6,9 @@ export default {
       await prisma.service.findOne({ where: { id } }).ansUser(),
     serviceimgfiles: async ({ id }, __, { prisma }) =>
       await prisma.serviceimgfiles.findMany({ where: { serviceId: id } }),
+    rating: async ({ id }, __, { prisma }) => {
+      console.log(id);
+      return await prisma.rating.findOne({ where: { serviceId: id } });
+    },
   },
 };

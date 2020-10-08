1da2dec9dc7f49d9ecbd5f538f33800ce38ac5a7
Choi JiYoul <wldbf97@naver.com>
2020-10-08 12:20:18 +0900

    seeMessage fix

--- src/api/Message/seeMessage/seeMessage.js
+++ src/api/Message/seeMessage/seeMessage.js
@@ -2,7 +2,6 @@ export default {
   Query: {
     messages: async (_, { room }, { request, isAuthenticated, prisma }) => {
       isAuthenticated(request.res.req);
-
       return await prisma.message.findMany({ where: { room } });
     },
   },
